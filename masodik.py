aca6a442d884e9436387486646e1ab34169391a6
Vörös Dávid <c2vordav@vasvari.org>
2025-09-29 09:26:36 +0200

    szét van szedve

---
+++ masodik.py
@@ -0,0 +1,7 @@
+def Teglalap():
+    a = float(input("Téglalap egyik oldala: "))
+    b = float(input("Téglalap másik oldala: "))
+
+    print(f"Téglalap területe: {a*b}\nA téglalap kerülete {2*(a+b)}")
+
+Teglalap()
