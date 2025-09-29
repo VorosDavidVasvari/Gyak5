3765a980985f24a30383cf3665a5fa70cb22f1bc
tihada <c2tihada@vasvari.org>
2025-09-29 09:11:24 +0200

    téglalap területe és kerülete számítás

---
+++ Main.py
@@ -0,0 +1,7 @@
+def Teglalap():
+    a = float(input("Téglalap egyik oldala: "))
+    b = float(input("Téglalap másik oldala: "))
+
+    print(f"A téglalap kerülete {2*(a+b)}\nTéglalap területe: {a*b}")
+
+Teglalap()
