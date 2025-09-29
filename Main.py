f160861587573b74aa530d8b6b295d46578c3412
Vörös Dávid <c2vordav@vasvari.org>
2025-09-29 09:22:48 +0200

    mindön kész von oszt csá

--- Main.py
+++ Main.py
@@ -1,12 +1,13 @@
 def Eggyenlotlenseg():
-    a: float = input("Kérem adja meg az a oldalt: ")
-    b: float = input("Kérem adja meg az b oldalt: ")
-    c: float = input("Kérem adja meg az c oldalt: ")
+    a: float = float(input("Kérem adja meg az a oldalt: "))
+    b: float = float(input("Kérem adja meg az b oldalt: "))
+    c: float = float(input("Kérem adja meg az c oldalt: "))
 
-    possible: bool = False
-    if not possible: possible = a + b > c
-    if not possible: possible = a + c > b
-    if not possible: possible = b + c > a
+    possible: bool = True
+
+    if possible: possible = (a + b) > c
+    if possible: possible = (a + c) > b
+    if possible: possible = (b + c) > a
 
     print(f"Ezekkel az oldalakkal{"" if possible else " nem"} lehetséges háromszöget csinálni!")
 
@@ -14,9 +15,9 @@ def Teglalap():
     a = float(input("Téglalap egyik oldala: "))
     b = float(input("Téglalap másik oldala: "))
 
-    print(f"A téglalap kerülete {2*(a+b)}\nTéglalap területe: {a*b}")
-
-Teglalap()
+    print(f"Téglalap területe: {a*b}\nA téglalap kerülete {2*(a+b)}")
 
 if __name__ == "__main__":
     Eggyenlotlenseg()
+    print()
+    Teglalap()
